b53f4ea73c3c79053e07650e77af1d559a23b542
SYSTEMS\amna.ghori <amna.ghori@systemsltd.com>
2022-07-07 10:40:58 +0500

    correction

--- src/main/java/com/awscicddemo/AWSCICDDemo/ItemController.java
+++ src/main/java/com/awscicddemo/AWSCICDDemo/ItemController.java
@@ -2,11 +2,8 @@ package com.awscicddemo.AWSCICDDemo;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:8080")
 public class ItemController {
 
 	@GetMapping("/home")
